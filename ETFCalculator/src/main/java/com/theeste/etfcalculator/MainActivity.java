b1bb5aad3d3bbbb69475eba527f8faad2236d65d
Ryan Esteves <snargledorf@gmail.com>
2014-06-06 16:45:05 -0400

    Stripped down to original app. No more My Devices, No Content provider.

--- ETFCalculator/src/main/java/com/theeste/etfcalculator/MainActivity.java
+++ ETFCalculator/src/main/java/com/theeste/etfcalculator/MainActivity.java
@@ -15,14 +15,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static android.app.ActionBar.Tab;
-
 public class MainActivity extends Activity implements
         DatePickerFragment.OnDateChangeListener,
-        CalculatorFragment.CalculatorFragmentCallbacks,
-        MyDevicesFragment.MyDeviceFragmentCallbacks {
-
-    private final String CURRENT_TAB = "current_tab";
+        CalculatorFragment.CalculatorFragmentCallbacks {
 
     private CharSequence mTitle;
 
@@ -43,71 +38,11 @@ public class MainActivity extends Activity implements
         setupActionBar();
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-
-        super.onSaveInstanceState(outState);
-
-        // Save the current tab
-        outState.putInt(CURRENT_TAB, getActionBar().getSelectedNavigationIndex());
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-
-        int selectedTab = savedInstanceState.getInt(CURRENT_TAB, 0);
-        selectTab(selectedTab);
-    }
-
     private void setupActionBar() {
         ActionBar actionBar = getActionBar();
         if (actionBar != null) {
-            setupActionBarTabs();
             actionBar.setTitle(mTitle);
         }
-    }
-
-    private void setupActionBarTabs() {
-        setupActionBarNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        addCalculatorTab();
-        addMyDevicesTab();
-    }
-
-    private void setupActionBarNavigationMode(int navigationMode) {
-        ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            actionBar.setNavigationMode(navigationMode);
-        }
-    }
-
-    private void addCalculatorTab() {
-        ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            Tab tab = actionBar.newTab()
-                    .setText(R.string.calculator)
-                    .setTabListener(new TabListener<CalculatorFragment>(
-                            this, CalculatorFragment.TAG, CalculatorFragment.class));
-            actionBar.addTab(tab);
-        }
-    }
-
-    private void addMyDevicesTab() {
-        ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            Tab tab = actionBar.newTab()
-                    .setText(R.string.my_devices_title)
-                    .setTabListener(new TabListener<MyDevicesFragment>(
-                            this, MyDevicesFragment.TAG, MyDevicesFragment.class));
-            actionBar.addTab(tab);
-        }
-    }
-
-    private void selectTab(int i) {
-        ActionBar actionBar = getActionBar();
-        if (actionBar == null)
-            return;
-        actionBar.setSelectedNavigationItem(i);
     }
 
     private void setupAds() {
@@ -136,12 +71,6 @@ public class MainActivity extends Activity implements
         if (calculatorFragment == null)
             return;
         calculatorFragment.setContractEndDate(year, month, day);
-    }
-
-    @Override
-    public void addDevice() {
-        Intent addDeviceIntent = new Intent(this, AddDeviceActivity.class);
-        startActivity(addDeviceIntent);
     }
 
     @Override
